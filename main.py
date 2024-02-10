c858ec163ac37842466557f93979ef8df9a8413f
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-02-10 13:18:20 +0530

    "Initializing the Streamlit"

--- main.py
+++ main.py
@@ -12,5 +12,19 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 # Function to load Gemini Pro Vision
 model = genai.GenerativeModel('gemini-pro-vision')
 
-# def get_gemini_response(input, image, prompt):
-#     response =
+
+def get_gemini_response(input, image, prompt):
+    response = model.generate_content([input, image[0], prompt])
+    return response.text
+
+
+# Initialize our Streamlit app
+
+st.set_page_config(page_title="MultiLanguage Invoice Extractor")
+st.header("Gemini Invoice Extractor")
+input = st.text_input("Input Prompt: ", key="input")
+uploaded_file = st.file_uploader("Choose an Image of the Invoice... ", type=["jpg", "jpeg", "png"])
+image = ""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
