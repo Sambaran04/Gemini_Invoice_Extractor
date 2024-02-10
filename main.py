b77bde8b36df9dc6fa6d627d716a2922ef6589aa
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-02-10 13:28:24 +0530

    "Our Project for Gemini Multilanguage Invoice Extractor is ready"

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ def get_gemini_response(input, image, prompt):
     return response.text
 
 
+def input_image_details(uploaded_file):
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mimeType": uploaded_file.type, # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
 # Initialize our Streamlit app
 
 st.set_page_config(page_title="MultiLanguage Invoice Extractor")
@@ -28,3 +42,16 @@ image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
+
+submit = st.button("Tell me about the Invoice")
+
+input_prompt = """You are an expert in understanding invoices. We will upload an image as invoice and you will have 
+to answer any questions based on the uploaded invoice image"""
+
+# If submit button is clicked
+
+if submit:
+    image_data = input_image_details(uploaded_file)
+    responses = get_gemini_response(input_prompt, image_data, input)
+    st.subheader("The Response is: ")
+    st.write(responses)
